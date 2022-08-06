ef77d25ecd33451e888ea63458ac6a2a965a2cf0
Salad5413 <zzy1354810313@outlook.com>
2022-08-05 23:02:45 -0400

    added the function of receiving and sending  messages to Azure Service Bus (in Json format)

--- agent.cs
+++ agent.cs
@@ -10,7 +10,11 @@ using System.Threading;
 using System.Security.Permissions;
 using System.Security.Principal;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Azure.Messaging.ServiceBus;
+
+
 
 namespace WindowsService1
 {
@@ -21,26 +25,217 @@ namespace WindowsService1
             InitializeComponent();
         }
 
+        ///////////////////////////////////////////////////////////
+        /// Server Bus initialize 
+
+        // connection string to your Service Bus namespace
+        static string connectionString = "Endpoint=sb://agentservice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=63qwPMHVfLnj82rHgCmFZaT9CsIwYhg1W8WGfZ3dym0=";
+
+        // name of Service Bus queue
+        static string queueSenderName = "sendAgentMessage";
+        static string queueReceiverName = "receiveAgentMessage";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+
+        // the sender used to receive messages from the queue
+        static ServiceBusReceiver receiver;
+
+        /*
+        // the processor that reads and processes messages from the queue
+        static ServiceBusProcessor processor;
+        */
+
+        protected override void OnStart(string[] args)
+        {
+            windowsService();
+        }
+
+        /* 
+         * 被托管机器本地Agent服务
+         * 
+         * 主功能一览：
+         * list: 列出本机所有进程及其Id
+         * k +（进程Id）：结束该Id对应的进程
+         * f + （进程名）：根据名称搜索进程
+         * kill_all: 结束上一次搜索找到的所有进程
+         * s + （进程名）：根据名称打开该应用程序
+         * 
+         * 需要手动启动/停止该Windows服务
+         * 
+         */
+        public static async Task windowsService()
+        {
+
+
+            int num_process = Process.GetProcesses().Length; // 获取进程总数，若没有任何进程，停止循环
+            Process[] searched_processes = new Process[1]; // 该array存储“f”指令的搜索结果
+
+            /*
+            await sendMessageToServerBus("\n***************************************\n" +
+                                             "list: 列出本机所有进程及其Id\n" +
+                                             "k + (进程Id)：结束该Id对应的进程\n" +
+                                             "f + (进程名)：根据名称搜索进程\n" +
+                                             "kill_all: 结束上一次搜索找到的所有进程" +
+                                             "s + (程序名) : 打开该应用程序" +
+                                             "\n***************************************\n");
+            */
+
+            // windows服务程序主体
+            while (num_process > 0)
+            {
+                string command = await receiveMessageFromServerBus(); // 从 server bus queue 读取指令
+
+                string com = "";
+                string info = "";
+
+                if (command.Contains(" "))
+                {
+                    com = command.Substring(0, command.IndexOf(" ")); // 提取指令
+                    info = command.Substring(command.IndexOf(" ") + 1, command.Length - command.IndexOf(" ") - 1); // 提取后续信息
+                }
+                else
+                {
+                    com = command;
+                }
+                
+
+                if (com.Equals("list")) // 列出本机所有进程及其Id
+                {
+                    await list_process();
+                }
+                else if (com.Equals("k")) // 结束该Id(info)对应的进程
+                {
+                    await kill_process(info);
+                }
+                else if (com.Equals("f")) // 根据名称(info)搜索进程
+                {
+                    searched_processes = await search_process(info);
+                }
+                else if (com.Equals("kill_all")) // 结束上一次搜索找到的所有进程
+                {
+                    if (searched_processes[0] != null) // 判断上一次是否搜索到结果
+                    {
+                        await kill_all(searched_processes); // 若有结果，结束所有进程
+                    }
+                    else
+                    {
+                        await sendMessageToServerBus("搜索结果为空，无法删除进程");
+                        continue;
+                    }
+                }
+                else if (com.Equals("s")) // 根据应用程序名称打开该程序进程
+                {
+                    Process.Start(info);
+                }
+                else // 若无法识别指令，弹出提醒并继续循环
+                {
+                    await sendMessageToServerBus($"Unknown Command: {com}");
+                }
+
+                // 更新进程总数
+                num_process = Process.GetProcesses().Length;
+            }
+        }
+
+        static async Task sendMessageToServerBus(string inputMessage)
+        {
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+
+            // Create a sender that we can use to send the message
+            sender = client.CreateSender(queueReceiverName);
+
+
+            JsonMessage jsonMessage = new JsonMessage { message = inputMessage };
+            string jsonMessageBody = JsonConvert.SerializeObject(jsonMessage);
+
+
+            // create a message that we can send. UTF-8 encoding is used when providing a string.
+            ServiceBusMessage jsonMessageSend = new ServiceBusMessage(jsonMessageBody);
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessageAsync(jsonMessageSend);
+                Console.WriteLine($"messages \"{inputMessage}\" has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+
+        static async Task<string> receiveMessageFromServerBus()
+        {
+            // Create the client object that will be used to create sender and receiver objects
+            client = new ServiceBusClient(connectionString);
+
+            // Create a receiver that we can use to receive the message
+            receiver = client.CreateReceiver(queueSenderName);
+
+
+            // Intialize ServiceBusReceivedMessage var to contain the received message
+            ServiceBusReceivedMessage receivedMessage;
+            
+
+            while (true)
+            {
+                // Receive message
+                receivedMessage = await receiver.ReceiveMessageAsync();
+
+                if(receivedMessage != null)
+                {
+                    JsonMessage jsonMessage = JsonConvert.DeserializeObject<JsonMessage>(receivedMessage.Body.ToString());
+
+                    // complete the message. message is deleted from the queue. 
+                    await receiver.CompleteMessageAsync(receivedMessage);
+
+                    // Calling DisposeAsync on client types is required to ensure that network
+                    // resources and other unmanaged objects are properly cleaned up.
+                    await receiver.DisposeAsync();
+                    await client.DisposeAsync();
+
+                    return jsonMessage.message;
+                }
+
+            }
+
+        }
+
         // 遍历并列出本机所有进程
-        private void list_process()
+        static private async Task list_process()
         {
             // 存储所有进程到 local_processes
             Process[] local_processes = Process.GetProcesses();
 
-            // 在console中列出所有进程
+            string allProcesses = "";
+            // 将所有进程添加至一个string
             foreach (Process p in local_processes)
             {
-                Console.WriteLine("{0}\t\tId:{1}", p, p.Id);
-
+                allProcesses += p.ProcessName;
+                allProcesses += "\t\tId: ";
+                allProcesses += p.Id.ToString();
+                allProcesses += "\n";
             }
-            Console.WriteLine("本机共有 {0} 个进程", local_processes.Length);
+            allProcesses += $"本机共有 {local_processes.Length} 个进程";
+
+            // send messages to server bus
+            await sendMessageToServerBus(allProcesses);
         }
+
 
         /* 根据 Id 结束进程
         * @param 需结束的进程Id
         * @return true 成功结束进程，false 未成功结束进程
         */
-        private bool kill_process(string id)
+        static private async Task kill_process(string id)
         {
             try // 尝试结束进程
             {
@@ -48,41 +243,48 @@ namespace WindowsService1
                 {
                     chosen.Kill();
                     chosen.WaitForExit();
-                    Console.WriteLine("{0}进程已结束", id);
-                    return true;
+                    await sendMessageToServerBus($"{id}进程已结束");
                 }
             }
             catch (Exception) // 若进程结束失败，弹出提示并结束方法
             {
-                Console.WriteLine("{0}进程结束失败",id);
-                return false;
+                await sendMessageToServerBus($"{id}进程结束失败");
             }
         }
+
 
         /* 根据名称搜索进程，列出在 console 上
         * @param 需搜索的进程名称
         * @return 搜索结果的 Array
         */
-        private Process[] search_process(string name)
+        static private async Task<Process[]> search_process(string name)
         {
             Process[] local_processes = Process.GetProcesses();
             Process[] result = new Process[local_processes.Length]; // 存储所有查询到的进程，并作为最后输出结果
             int i = 0; // 记录找到的结果个数
 
-            Console.WriteLine("");
+            string processesList = "";
 
             foreach (Process p in local_processes) // 遍历所有进程名称，将任何名称中包含"name"字符串、
-                                                   // 的进程存储到Array“result”
+                                                   // 的进程存储到Array“result”并将列表发送至server
             {
                 if(p.ProcessName.ToLower().Contains(name.ToLower()))
                 {
                     result[i] = p;
                     i++;
-                    Console.WriteLine("{0}\t\t\tId:{1}", p, p.Id);
+                    processesList += p.ProcessName;
+                    processesList += "\t\tId: ";
+                    processesList += p.Id.ToString();
+                    processesList += "\n";
                 }
             }
-            //输出结果
-            Console.WriteLine("\n共找到 {0} 个结果", i);
+
+            // 输出结果
+            processesList += $"\n共找到 {i} 个结果";
+
+            // 将列表发送至server
+            await sendMessageToServerBus(processesList);
+
             return result;
         }
 
@@ -90,21 +292,31 @@ namespace WindowsService1
         * @param 需结束的进程 Array
         * @return true 成功结束所有进程，false 未成功结束所有进程
         */
-        private bool kill_all(Process[] p_array)
+        static private async Task kill_all(Process[] p_array)
         {
             // 二次确认是否结束所有进程
-            Console.WriteLine("是否结束以下进程？");
+
+            string doubleCheckMessage = "是否结束以下进程？";
+
             foreach (Process p in p_array) 
             {   
                 if(p != null)
                 {
-                    Console.WriteLine("{0}\t\tId:{1}", p.ProcessName, p.Id);
+                    doubleCheckMessage += p.ProcessName;
+                    doubleCheckMessage += "\t\tId: ";
+                    doubleCheckMessage += p.Id.ToString();
+                    doubleCheckMessage += "\n";
                 }
             }
-            Console.WriteLine("[Y/n]");//输入“Y”确定，输入其他任何字符则取消
-            string choice = Console.ReadLine();
+            doubleCheckMessage += "[Y/n]";  //输入“Y”确定，输入其他任何字符则取消
 
-            if(choice.Equals("Y")) // 确定并尝试结束所有上一次搜索结果中的进程
+            // 发送二次确认消息至 server
+            await sendMessageToServerBus(doubleCheckMessage);
+
+            // 等待接收二次确认消息
+            string choice = await receiveMessageFromServerBus();
+
+            if (choice.Equals("Y")) // 确定并尝试结束所有上一次搜索结果中的进程
             {
                 try
                 {
@@ -116,92 +328,26 @@ namespace WindowsService1
                             p.WaitForExit();
                         }
                     }
-                    Console.WriteLine("进程已结束"); // 所有进程成功结束
-                    return true;
+                    await sendMessageToServerBus("进程已结束"); // 所有进程成功结束
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("结束进程失败"); // 所有（部分）进程未能结束
-                    return false;
+                    await sendMessageToServerBus("结束进程失败"); // 所有（部分）进程未能结束
                 }
-            }
-            else // 取消并结束方法
-            { 
-                return false; 
             }
         }
 
-
-        protected override void OnStart(string[] args)
+        // string 转成 json
+        static JObject toJson(string str)
         {
-            /* 
-             * 第一周任务：被托管机器本地Agent服务
-             * 
-             * 主功能一览：
-             * list: 列出本机所有进程及其Id
-             * k +（进程Id）：结束该Id对应的进程
-             * f + （进程名）：根据名称搜索进程
-             * kill_all: 结束上一次搜索找到的所有进程
-             * s + （进程名）：根据名称打开该应用程序
-             * 
-             * 需要手动启动/停止该Windows服务
-             * 
-             */
-
-            int num_process = Process.GetProcesses().Length; // 获取进程总数，若没有任何进程，停止循环
-            Process[] searched_processes = new Process[1]; // 该array存储“f”指令的搜索结果
-            while (num_process > 0)
-            {
-                Console.WriteLine("\n***************************************\n" +
-                                  "list : 列出本机所有进程及其Id\n" +
-                                  "k + (进程Id)：结束该Id对应的进程\n" +
-                                  "f + (进程名)：根据名称搜索进程\n" +
-                                  "kill_all : 结束上一次搜索找到的所有进程" +
-                                  "s + (程序名) : 打开该应用程序" +
-                                  "\n***************************************\n");
-
-
-                string command = Console.ReadLine(); // 读取用户输入
-                string com = command.Substring(0, command.IndexOf(" ")); // 提取指令
-                string info = command.Substring(command.IndexOf(" ") + 1, command.Length - command.IndexOf(" ") - 1); // 提取后续信息
-
-                if (com.Equals("list")) // 列出本机所有进程及其Id
-                {
-                    list_process();
-                }
-                else if (com.Equals("k")) // 结束该Id(info)对应的进程
-                {
-                    kill_process(info);
-                }
-                else if (com.Equals("f")) // 根据名称(info)搜索进程
-                {
-                    searched_processes = search_process(info);
-                }
-                else if (com.Equals("kill_all")) // 结束上一次搜索找到的所有进程
-                {
-                    if (searched_processes[0] != null) // 判断上一次是否搜索到结果
-                    {
-                        kill_all(searched_processes); // 若有结果，结束所有进程
-                    }
-                    else
-                    {
-                        Console.WriteLine("搜索结果为空"); // 若结果为空，则不结束任何进程
-                        continue;
-                    }
-                }
-                else if (com.Equals("s")) // 根据应用程序名称打开该程序进程
-                {
-                    Process.Start(info);
-                }
-                else // 若无法识别指令，弹出提醒并继续循环
-                {
-                    Console.WriteLine("Unknown Command: {0}", com);
-                }
-
-                // 更新进程总数
-                num_process = Process.GetProcesses().Length;
-            }
+            JObject jsonResult = JObject.Parse(str);
+            return jsonResult;
         }
+
+
+
+
+
 
         protected override void OnStop()
         {
